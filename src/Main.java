1363f5f83f57c76ded071d09d4605503bc170fa1
AnCapone <okai.kot44@gmail.com>
2024-04-14 13:54:50 +0300

    implement all actions with figures. Random generation for figures and calculate total area

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import figures.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        someFiguresMagic();
+
+    }
+
+    public static void someFiguresMagic() {
+        ArrayList<GeomFigure> figures = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            figures.add(new SquareFigure(random.nextInt(10)));
+            figures.add(new Triangle(random.nextInt(10), random.nextInt(10)));
+            figures.add(new Round(random.nextInt(10)));
+        }
+
+        double totalArea = 0;
+        for (GeomFigure figure : figures) {
+            totalArea += figure.area();
+        }
+        System.out.println("Total area: " + totalArea);
     }
 }
